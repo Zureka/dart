a5b98806abbb4ee6164038377978f81e6a68bcf2
Alex Zurek <zureka@mail.gvsu.edu>
2019-10-08 21:01:25 -0400

    Auto format new exercise.dart file

--- exercises/beer-song/lib/example.dart
+++ exercises/beer-song/lib/example.dart
@@ -7,21 +7,16 @@ class BeerSong {
 
       if (num > 2) {
         verses.add("$num bottles of beer on the wall, $num bottles of beer.");
-        verses.add(
-            "Take one down and pass it around, ${num - 1} bottles of beer on the wall.");
+        verses.add("Take one down and pass it around, ${num - 1} bottles of beer on the wall.");
       } else if (num == 2) {
         verses.add("2 bottles of beer on the wall, 2 bottles of beer.");
-        verses.add(
-            "Take one down and pass it around, 1 bottle of beer on the wall.");
+        verses.add("Take one down and pass it around, 1 bottle of beer on the wall.");
       } else if (num == 1) {
         verses.add("1 bottle of beer on the wall, 1 bottle of beer.");
-        verses.add(
-            "Take it down and pass it around, no more bottles of beer on the wall.");
+        verses.add("Take it down and pass it around, no more bottles of beer on the wall.");
       } else if (num == 0) {
-        verses.add(
-            "No more bottles of beer on the wall, no more bottles of beer.");
-        verses.add(
-            "Go to the store and buy some more, 99 bottles of beer on the wall.");
+        verses.add("No more bottles of beer on the wall, no more bottles of beer.");
+        verses.add("Go to the store and buy some more, 99 bottles of beer on the wall.");
       }
 
       if (numVerses > 1 && i != numVerses - 1) {
